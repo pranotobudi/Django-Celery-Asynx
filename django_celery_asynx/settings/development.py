55ee37bfbd10b93d2fed278edc357cf785f134a9
pranotobudi <pranotobudi.s@gmail.com>
2019-04-05 16:28:37 +0700

    init django project and postgreSQL settings

---
+++ django_celery_asynx/settings/development.py
@@ -0,0 +1,22 @@
+.base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoceleryasynx',
+        'USER': 'djangoceleryasynx',
+        'PASSWORD': 'passworddjangoceleryasynx',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
