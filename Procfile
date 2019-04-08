c6d4170c787b9eed80b8b88baf5a6de864ed0d43
pranotobudi <pranotobudi.s@gmail.com>
2019-04-08 11:11:32 +0700

    add celery settings for production

--- Procfile
+++ Procfile
@@ -1,2 +1,4 @@
 web: gunicorn django_celery_asynx.wsgi --log-file -
 release: python manage.py migrate
+worker: python manage.py celery worker -B -l info
+beat: python manage.py celery beat –loglevel=info
---
+++ django_celery_asynx/celeryconfig-production.py
@@ -0,0 +1,7 @@
+CELERY_BROKER_URL = 'amqp://qdgzpakt:abjU0ZWeBWyI48SG6SwQTLOe_hJzRWYg@orangutan.rmq.cloudamqp.com/qdgzpakt'
+CELERY_RESULT_BACKEND = 'amqp://qdgzpakt:abjU0ZWeBWyI48SG6SwQTLOe_hJzRWYg@orangutan.rmq.cloudamqp.com/qdgzpakt'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_IMPORT = ('myapp',)
+CELERY_BROKER_POOL_LIMIT = 1 #for heroku free dyno
